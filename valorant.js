fd18234f73b422810e43dab62282a0e3193bb54c
daolvera <dancda8@byu.edu>
2021-10-15 09:13:00 -0600

    10/15 morning

--- valorant.js
+++ valorant.js
@@ -1,16 +1,45 @@
-// header("Access-Control-Allow-Origin: *");
-// const options = {method: 'GET', headers: {Accept: 'application/json'}};
+const skinUrl = ('https://valorant-api.com/v1/weapons/skins')
+const agentUrl = ("https://valorant-api.com/v1/agents");
 
-// fetch('https://api.pandascore.co/valorant/teams?sort=&page=1&per_page=50', options)
+
+// fetch(mapsUrl)
 //   .then(response => response.json())
 //   .then(response => console.log(response))
 //   .catch(err => console.error(err));
-const skinUrl = ('https://valorant-api.com/v1/weapons/skins')
-const mapsUrl = ("https://valorant-api.com/v1/maps");
-const agentUrl = ("https://valorant-api.com/v1/agents");
-fetch(agentUrl)
-  .then(response => response.json())
-  .then(response => console.log(response))
-  .catch(err => console.error(err));
 
+//MAPS
+const mapsUrl = ("https://valorant-api.com/v1/maps");
+const mapsNav = document.querySelector(".map-nav");
+const mapMain = document.querySelector(".map-main")
+var mapsButton = document.querySelector(".map-nav-item");
+
+function loadMap(mapIndex){
+  fetch(mapsUrl)
+  .then(response => response.json())
+  .then(response => {
+    var tempMapName = response["data"][mapIndex].displayName;
+    var tempMapImg = response["data"][mapIndex].splash;
+    var tempMapHTML;
+    tempMapHTML += "<div class='map-image'>" + tempMapImg + "</div>";
+    tempMapHTML += "<div class='map-callouts'>" + tempMapName + "</div>";
+    mapMain.innerHTML = tempMapHTML;
+  });
+};
+
+window.addEventListener("load", function() {
+  fetch(mapsUrl)
+  .then(response => response.json())
+  .then(response => {
+    console.log(response);
+    for (num in response["data"]){
+      if (num != 6){
+        var tempMap = response["data"][num].displayName;
+        mapsNav.innerHTML += `<button class = 'map-nav-item' onClick ='loadMap(${num})'>` + tempMap + "</button>";
+      }
+
+    }
+
+  })
+  .catch(err => console.error(err));
+});
  
