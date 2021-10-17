0445d9a7d9f29a57cd2fd2219a051dac1d52ef96
daolvera <dancda8@byu.edu>
2021-10-16 18:25:41 -0600

    10/16 before dinner

--- guns.js
+++ guns.js
@@ -2,14 +2,16 @@ const skinUrl = ('https://valorant-api.com/v1/weapons/skins');
 const gunUrl = ('https://valorant-api.com/v1/weapons');
 const gunMain = document.querySelector(".gun-main");
 
-function changeSkin(gunIndex, skinIndex){
-	fetch(gunUrl)
-	.then(response => response.json())
-	.then(response => {
-		var gunImg =  response["data"][gunIndex]["skins"][skinIndex].displayIcon;
-		return gunImg;
-	});
-};
+
+function changeSkin(gunIndex){
+	var selectSkin = document.querySelector(`.temp-skin-${gunIndex}`);
+	var selection = selectSkin.selectedIndex;
+	var newImg = selectSkin.options[selection].value;
+	// console.log(newImg);
+	var imageToChange = document.querySelector(`#change_${gunIndex}`);
+	// console.log(imageToChange);
+	imageToChange.src = newImg;
+}
 
 function loadGun(gunIndex){
 	fetch(gunUrl)
@@ -17,15 +19,29 @@ function loadGun(gunIndex){
 	.then(response => {
 	  var tempGunName = response["data"][gunIndex].displayName;
 	  var tempGunImg = response["data"][gunIndex].displayIcon;
+	  var tempGunSkinList = response["data"][gunIndex]["skins"];
+	  var tempGunSkinHTML = `<div class='skin-selector'> <label for ='temp-skin-${gunIndex}'></label>`;
+	  tempGunSkinHTML += `<select class='temp-skin-${gunIndex}' name='Skin' onchange='changeSkin(${gunIndex})'>`;
+	  tempGunSkinHTML += `<option value ='default'> Default </option>`;
+	  for (numSkin in tempGunSkinList){
+		var tempGunSkin = response["data"][gunIndex]["skins"][numSkin].displayName;
+		var tempGunSkinLink = response["data"][gunIndex]["skins"][numSkin].displayIcon;
+		tempGunSkinHTML += `<option value ='${tempGunSkinLink}'> ${tempGunSkin} </option>`;
+	  }
+	  tempGunSkinHTML += "</select> </div>";
+
 	  var tempGunHTML;
 	  tempGunHTML = '<div class ="gun-item">';
 	  tempGunHTML += "<div class='gun-name'> <h3>" + tempGunName + "</h3> </div> ";
-	  var skinIndex = 0;
+	  tempGunHTML += tempGunSkinHTML;
 	  if (gunIndex > 6 && gunIndex < 12){
-		tempGunHTML += `<input type='image' class='gun-image-small' id='${gunIndex}-basic' src = ${tempGunImg}  onClick='changeSkin(${tempGunImg}, ${skinIndex})'>`;  
+		tempGunHTML += `<div class='gun-image-small'> <img id ='change_${gunIndex}' src = ${tempGunImg}  >`;  
+	  }
+	  else if (gunIndex == 17){
+		tempGunHTML += `<div class='gun-image-knife'> <img id ='change_${gunIndex}' src = ${tempGunImg}  >`;  
 	  }
 	  else{
-	  	tempGunHTML += `<input type='image' class='gun-image' src= ${tempGunImg} onClick= 'changeSkin(${gunIndex},  0)' >`;
+	  	tempGunHTML += `<div class='gun-image'> <img id='change_${gunIndex}'src= ${tempGunImg} >`;
 	  }
 	  tempGunHTML += "</div>";
 	  gunMain.innerHTML += tempGunHTML;
@@ -44,8 +60,7 @@ window.addEventListener("load", function() {
 fetch(gunUrl)
 .then(response => response.json())
 .then(response => {
-	console.log(response)
-	// default Agent Loaded
+	// console.log(response)
 	for( var i in response["data"]){
 		loadGun(i);
 	}
